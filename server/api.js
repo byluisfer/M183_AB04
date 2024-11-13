b4ff697954f711814f3a3157c38954f2467a9aad
Luis Fernandez <luisferdass@gmail.com>
2024-11-13 11:56:54 +0100

    commit

--- server/api.js
+++ server/api.js
@@ -1,4 +1,11 @@
 import db from "./database.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import env from "dotenv";
+
+env.config();
+
+const SECRET_KEY = process.env.SECRET_KEY;
 
 const initializeAPI = async (app) => {
   app.post("/api/login", login);
@@ -6,6 +13,26 @@ const initializeAPI = async (app) => {
 
 const login = async (req, res) => {
   const { username, password } = req.body;
+
+  db.get(`SELECT * FROM users WHERE username = ?`, [username], (err, row) => {
+    if (err) {
+      res.status(500).send("Error to conect to DB");
+    } else if (!user) {
+      res.status(401).send("User not found");
+    } else {
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (result) {
+          const token = jwt.sign({ username },
+            SECRET_KEY,
+            { expiresIn: "1h" }
+          );
+          res.json({ token })
+        } else {
+          res.status(401).send("Wrong password or username");
+        }
+      })
+    }
+  })
 
   const answer = `
     <h1>Answer</h1>
